5644387943eceecfd999d849c96b9611f81a2045
Beteab <beteabtefera@yahoo.com>
2025-01-11 03:32:56 -0600

    deploy

--- colorstack-api-dashboard/components/Dashboard.tsx
+++ colorstack-api-dashboard/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client' 
 import React, { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '@/contexts/AuthContext';
 import { useRouter } from 'next/navigation';
