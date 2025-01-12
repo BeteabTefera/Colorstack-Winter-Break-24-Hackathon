4dc360538be9a80bb8b0d0cf63a42230df07abde
Beteab Tefera <beteabtefera@yahoo.com>
2025-01-11 19:46:46 -0600

    Update page.tsx

--- colorstack-api-dashboard/app/page.tsx
+++ colorstack-api-dashboard/app/page.tsx
@@ -4,3 +4,11 @@ export default function Home() {
   return <LandingPage />
 }
 
+// This function gets called at build time
+export async function getStaticProps() {
+  // You can fetch data here if needed for the landing page
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
